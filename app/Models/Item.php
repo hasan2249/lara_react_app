9b91744b105a5bd12e88baff83698961c3cf5278
Hasan Jahjah <hasanjahjah224@gmail.com>
2022-08-30 15:50:00 +0300

    Add Item model and Create its table in DB

---
+++ app/Models/Item.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Relationships\ItemsRelationships;
+
+class Item extends Model
+{
+    use HasFactory, ItemsRelationships;
+
+    protected $fillable = ['name', 'description', 'disount', 'catygory_id', 'price'];
+}
